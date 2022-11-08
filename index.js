9033dd70a15503b7e58866a99cf9e2a881e84a08
khsultana <khsultana1995@gmail.com>
2022-11-09 02:07:56 +0600

    new

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ybtjl3s.mongodb.net/?retryWrites=true&w=majority`;
-const uri =
-  "mongodb+srv://$Kh_Sultana_Kitchen:5e9EHkaJMuu4kUb2@cluster0.ybtjl3s.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ybtjl3s.mongodb.net/?retryWrites=true&w=majority`;
+// const uri =
+//   "mongodb+srv://$Kh_Sultana_Kitchen:5e9EHkaJMuu4kUb2@cluster0.ybtjl3s.mongodb.net/?retryWrites=true&w=majority";
 console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
